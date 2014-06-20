7698314e7830dca50213c5631092dc42757da4a9
Pjotr Prins <pjotr.public01@thebird.nl>
2014-06-20 09:37:02 +0200

    More tests

--- features/sfilter.feature
+++ features/sfilter.feature
@@ -12,17 +12,31 @@ Feature: Sample filters
 1 10723 . C G 73.85 . AC=4;AF=0.667;AN=6;BaseQRankSum=1.300;DP=18;Dels=0.00;FS=3.680;HaplotypeScore=0.0000;MLEAC=4;MLEAF=0.667;MQ=20.49;MQ0=11;MQRankSum=1.754;QD=8.21;ReadPosRankSum=0.000 GT:AD:DP:GQ:PL
     """
     When I evaluate '0/0:6,0:6:3:0,3,33'
+    Then I expect s.empty? to be false
     Then I expect s.dp to be 6
     And sfilter 's.dp>4' to be true
 
   Scenario: Sample with missing data
 
-    When I evaluate '0/0:6,0:.:3:0,3,33'
+    When I evaluate missing '0/0:6,0:.:3:0,3,33'
+    Then I expect s.empty? to be false
+    Then I expect s.dp to be nil
+    And sfilter 's.dp>4' to throw an error
+
+    When I evaluate missing '0/0:6,0:.:3:0,3,33' with ignore missing
+    Then I expect s.empty? to be false
     Then I expect s.dp to be nil
     And sfilter 's.dp>4' to be false
 
   Scenario: Missing sample
-    When I evaluate './.'
+    When I evaluate empty './.'
+    Then I expect s.empty? to be true
+    Then I expect s.dp to throw an error
+    And sfilter 's.dp>4' to throw an error
+
+    When I evaluate empty './.' with ignore missing
+    Then I expect s.empty? to be true
     Then I expect s.dp to be nil
     And sfilter 's.dp>4' to be false
+
 
--- features/step_definitions/sfilter.rb
+++ features/step_definitions/sfilter.rb
@@ -1,9 +1,12 @@
 Given(/^the VCF line$/) do |string|
-  pending # express the regexp above with the code you wish you had
+  @header = nil
+  @vcfline = string
 end
 
-When(/^I evaluate '(\d+)\/(\d+):(\d+),(\d+):(\d+):(\d+):(\d+),(\d+),(\d+)'$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9|
-  pending # express the regexp above with the code you wish you had
+When(/^I evaluate '([^']+)'$/) do |arg1|
+  @fields = VcfLine.parse((@vcfline.split(/\s+/)+[arg1]).join("\t"))
+  @rec1 = VcfRecord.new(@fields,@header)
+  p @rec1
 end
 
 Then(/^I expect s\.dp to be (\d+)$/) do |arg1|
@@ -14,7 +17,7 @@ Then(/^sfilter 's\.dp>(\d+)' to be true$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-When(/^I evaluate '(\d+)\/(\d+):(\d+),(\d+):\.:(\d+):(\d+),(\d+),(\d+)'$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8|
+When(/^I evaluate missing '(\d+)\/(\d+):(\d+),(\d+):\.:(\d+):(\d+),(\d+),(\d+)'$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8|
   pending # express the regexp above with the code you wish you had
 end
 
@@ -26,7 +29,31 @@ Then(/^sfilter 's\.dp>(\d+)' to be false$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-When(/^I evaluate '\.\/\.'$/) do
+When(/^I evaluate empty '\.\/\.'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I expect s\.empty\? to be false$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^sfilter 's\.dp>(\d+)' to throw an error$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I evaluate missing '(\d+)\/(\d+):(\d+),(\d+):\.:(\d+):(\d+),(\d+),(\d+)' with ignore missing$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I expect s\.empty\? to be true$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I expect s\.dp to throw an error$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I evaluate empty '\.\/\.' with ignore missing$/) do
   pending # express the regexp above with the code you wish you had
 end
 
