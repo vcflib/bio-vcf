d96d293adb3f6f486408696c8434915fa0b3dd26
Pjotr Prins <pjotr.public01@thebird.nl>
2014-08-29 12:32:25 +0200

    Adding statistics

---
+++ lib/bio-vcf/vcfstatistics.rb
@@ -0,0 +1,28 @@
+module BioVcf
+
+  class VcfStatistics
+
+    def initialize
+      @count = 0
+      @ref_alt_count = {}
+    end
+
+    def add rec
+      @count += 1
+      s = rec.ref+">"+rec.alt[0]
+      @ref_alt_count[s] ||= 0
+      @ref_alt_count[s] += 1
+    end
+
+    def print
+      puts "==== Statistics ====================================================="
+      @ref_alt_count.each do |k,v|
+        printf k+"\t%d\t%2.0d%%\n",v,(v.to_f/@count*100).round
+      end
+      puts "Total\t#{@count}"
+      puts "====================================================================="
+    end
+  end
+
+end
+
