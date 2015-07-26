d481cd71353650d6bd1fbc10d0a50b011e74cb23
Pjotr Prins <pjotr.public01@thebird.nl>
2015-07-26 17:23:18 +0200

    PCOWS

--- lib/bio-vcf/pcows.rb
+++ lib/bio-vcf/pcows.rb
@@ -2,26 +2,28 @@
 
 class PCOWS
 
+  RUNNINGEXT = 'part'
+  
   def initialize(num_threads,name=__FILE__)
+    num_threads = 1 if !num_threads # FIXME: set to cpu_num by default
     @num_threads = num_threads
     @thread_list = []
     @name = name
     @tmpdir =  Dir::mktmpdir(@name+'_')
-    @count = 0
   end
 
-  # Feed the func and state to COWS. Note that func is a closure so it
-  # can pick up surrounding scope at invocation in addition to the
-  # data captured in 'state'.
+  # Feed the worker func and state to COWS. Note that func is a
+  # closure so it can pick up surrounding scope at invocation in
+  # addition to the data captured in 'state'.
   
-  def run(func,state,tmpdir,num_threads)
+  def worker(func,state)
     pid = nil
-    if num_threads and num_threads>1
-      @count += 1
-      threadfilen = mktmpfilename(tmpdir,@count)
+    if @num_threads>1
+      count = @pid_list.size+1
+      threadfilen = mktmpfilename(count)
       pid = fork do
         # ---- This is running a new copy-on-write process
-        tempfn = threadfilen+'.part'
+        tempfn = threadfilen+'.'+RUNNINGEXT
         STDOUT.reopen(File.open(tempfn, 'w+'))
         func.call(state)
         STDOUT.flush
@@ -33,50 +35,46 @@ class PCOWS
       # ---- Call in main process
       func.call(state)
     end
-    @thread_list << [ pid,@count,threadfilen ]
+    @pid_list << [ pid,count,threadfilen ]
     return true
   end
 
   # Make sure no more than num_threads are running at the same time -
   # this is achieved by checking the PID table and the running files
   # in the tmpdir
-  def wait_for_threadpool(thread_list, num_threads)
+
+  def wait_for_threadpool(pid_list)
     while true
       # ---- count running pids
-      running = thread_list.reduce(0) do | sum, thread_info |
-        if thread_info[0] && pid_running?(thread_info[0])
+      running = pid_list.reduce(0) do | sum, info |
+        [pid,count,pidfn] == info
+        if pid && pid_running?(pid)
           sum+1
-        elsif thread_info[0]==nil && File.exist?(thread_info[1]+'.part')
+        elsif File.exist?(pidfn+'.'+RUNNINGEXT)
           sum+1
         else
           sum
         end
       end
-      break if running < num_threads
+      break if running < @num_threads
       sleep 0.1
     end
   end
 
-  def pid_running?(pid)
-    begin
-      fpid,status=Process.waitpid2(pid,Process::WNOHANG)
-    rescue Errno::ECHILD, Errno::ESRCH
-      return false
-    end
-    return true if nil == fpid && nil == status
-    return ! (status.exited? || status.signaled?)
-  end
   
-  # ---- In this section the output gets collected and printed on STDOUT
+  # ---- In this section the output gets collected and passed on to a
+  #      printer thread. Make sure the printing is ordered and that
+  #      no printers are running at the same time. The printer thread
+  #      should be doing as little processing as possible.
   def cleanup
     if options[:num_threads]
       STDOUT.reopen(orig_std_out)
-      $stderr.print "Final pid=#{thread_list.last[0]}, size=#{lines.size}\n"
+      $stderr.print "Final pid=#{pid_list.last[0]}, size=#{lines.size}\n"
       lines = []
 
       fault = false
       # Wait for the running threads to complete
-      thread_list.each do |info|
+      pid_list.each do |info|
         (pid,threadfn) = info
         tempfn = threadfn + '.running'
         timeout = 180
@@ -104,7 +102,7 @@ class PCOWS
         end
       end
       # Collate the output
-      thread_list.each do | info |
+      pid_list.each do | info |
         (pid,fn) = info
         if !fault
           # This should never happen
@@ -121,10 +119,20 @@ class PCOWS
     end
   end  # cleans up tempdir
 
-  protected
+  private
   
   def mktmpfilename(num)
     @tmpdir+sprintf("/%0.6d-",num)+@name
+  end
+
+  def pid_running?(pid)
+    begin
+      fpid,status=Process.waitpid2(pid,Process::WNOHANG)
+    rescue Errno::ECHILD, Errno::ESRCH
+      return false
+    end
+    return true if nil == fpid && nil == status
+    return ! (status.exited? || status.signaled?)
   end
 
 end
