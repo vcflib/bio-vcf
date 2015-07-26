9929b11e4d6ca697aa53272f96acfabd096969a2
Pjotr Prins <pjotr.public01@thebird.nl>
2015-07-26 18:23:48 +0200

    PCOWS in poc

--- lib/bio-vcf/pcows.rb
+++ lib/bio-vcf/pcows.rb
@@ -7,9 +7,11 @@ class PCOWS
   def initialize(num_threads,name=__FILE__)
     num_threads = 1 if !num_threads # FIXME: set to cpu_num by default
     @num_threads = num_threads
-    @thread_list = []
+    @pid_list = []
     @name = name
     @tmpdir =  Dir::mktmpdir(@name+'_')
+    @last_output = 0 # counter
+    @output_locked = nil
   end
 
   # Feed the worker func and state to COWS. Note that func is a
@@ -20,10 +22,10 @@ class PCOWS
     pid = nil
     if @num_threads>1
       count = @pid_list.size+1
-      threadfilen = mktmpfilename(count)
+      fn = mktmpfilename(count)
       pid = fork do
         # ---- This is running a new copy-on-write process
-        tempfn = threadfilen+'.'+RUNNINGEXT
+        tempfn = fn+'.'+RUNNINGEXT
         STDOUT.reopen(File.open(tempfn, 'w+'))
         func.call(state)
         STDOUT.flush
@@ -35,7 +37,7 @@ class PCOWS
       # ---- Call in main process
       func.call(state)
     end
-    @pid_list << [ pid,count,threadfilen ]
+    @pid_list << [ pid,count,fn ]
     return true
   end
 
@@ -43,14 +45,12 @@ class PCOWS
   # this is achieved by checking the PID table and the running files
   # in the tmpdir
 
-  def wait_for_threadpool(pid_list)
+  def wait_for_threadpool()
     while true
       # ---- count running pids
-      running = pid_list.reduce(0) do | sum, info |
-        [pid,count,pidfn] == info
-        if pid && pid_running?(pid)
-          sum+1
-        elsif File.exist?(pidfn+'.'+RUNNINGEXT)
+      running = @pid_list.reduce(0) do | sum, info |
+        (pid,count,fn) == info
+        if pid_or_file_running?(pid,fn)
           sum+1
         else
           sum
@@ -61,28 +61,48 @@ class PCOWS
     end
   end
 
-  
   # ---- In this section the output gets collected and passed on to a
-  #      printer thread. Make sure the printing is ordered and that
-  #      no printers are running at the same time. The printer thread
-  #      should be doing as little processing as possible.
-  def cleanup
-    if options[:num_threads]
-      STDOUT.reopen(orig_std_out)
-      $stderr.print "Final pid=#{pid_list.last[0]}, size=#{lines.size}\n"
-      lines = []
+  #      printer thread. This function makes sure the printing is
+  #      ordered and that no printers are running at the same
+  #      time. The printer thread should be doing as little processing
+  #      as possible.
 
-      fault = false
-      # Wait for the running threads to complete
-      pid_list.each do |info|
-        (pid,threadfn) = info
-        tempfn = threadfn + '.running'
-        timeout = 180
-        if (pid && !pid_running?(pid)) || fault
-          # no point to wait for a long time if we've failed one already or the proc is dead
-          timeout = 1
+  def process_output(func)
+    if @output_locked
+      (pid,count,fn) = @output_locked
+      return if File.exist?(fn)  # still processing
+      # on to the next one
+      @last_output += 1
+      @output_locked = nil
+    end
+    if info = @pid_list[@last_output]
+      (pid,count,fn) = info
+      if File.exist?(fn)
+        # Yes! We have the next output, create outputter
+        pid = fork do
+          File.new(fn).each_line { |buf|
+            func.call(buf)
+          }
+          File.unlink(fn)
+          exit(0)
         end
-        $stderr.print "Waiting up to #{timeout/60} minutes for pid=#{pid} to complete\n"
+        @output_locked = info
+      end
+    end
+  end
+
+  # This is the final cleanup after the reader thread is done. All workers
+  # need to complete.
+  
+  def wait_for_children_to_complete
+    $stderr.print "Final pid=#{pid_list.last[0]}\n"
+    # Wait for the running threads to complete
+    pid_list.each do |info|
+      (pid,count,threadfn) = info
+      tempfn = threadfn + '.'+RUNNINGEXT
+      if pid_or_file_running?(pid)
+        timeout = 180
+        $stderr.print "Waiting up to #{timeout} seconds for pid=#{pid} to complete\n"
         begin
           Timeout.timeout(timeout) do
             while not File.exist?(threadfn)  # wait for the result to appear
@@ -97,34 +117,22 @@ class PCOWS
             Process.kill 9, pid
             Process.wait pid
           end
-          $stderr.print "FATAL: child process killed because it stopped responding, pid = #{pid}\n"
-          fault = true
+          raise "FATAL: child process killed because it stopped responding, pid = #{pid}\n"
         end
       end
-      # Collate the output
-      pid_list.each do | info |
-        (pid,fn) = info
-        if !fault
-          # This should never happen
-          raise "FATAL: child process output #{fn} is missing" if not File.exist?(fn)
-          $stderr.print "Reading #{fn}\n"
-          File.new(fn).each_line { |buf|
-            print buf
-          }
-          File.unlink(fn)
-        end
-        Process.wait(pid) if pid && pid_running?(pid)
-      end
-      return 1 if fault
     end
-  end  # cleans up tempdir
+  end
 
   private
   
-  def mktmpfilename(num)
-    @tmpdir+sprintf("/%0.6d-",num)+@name
+  def mktmpfilename(num,ext=nil)
+    @tmpdir+sprintf("/%0.6d-",num)+@name+(ext ? '.'+ext : '')
   end
-
+  
+  def pid_or_file_running?(pid,fn)
+    (pid && pid_running?(pid)) or File.exist?(fn+'.'+RUNNINGEXT)
+  end
+  
   def pid_running?(pid)
     begin
       fpid,status=Process.waitpid2(pid,Process::WNOHANG)
