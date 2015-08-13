d46b31278cdcad92a668cc13eda63a5cbfdd26e7
Pjotr Prins <pjotr.public01@thebird.nl>
2015-08-13 10:55:20 +0200

    Phasing out bundler and jeweler

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,3 @@
-require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
-
 # require 'mini/test'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
